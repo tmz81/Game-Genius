d10ea391753d5e5f0c8215964fa4e87d3b7de7e3
tmz81 <tmz.zt4@gmail.com>
2022-04-06 19:22:21 -0300

    🚀

--- src/app.js
+++ src/app.js
@@ -1,21 +1,12 @@
 const { app, BrowserWindow,  } = require ('electron');
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 600,
-    height: 600,
-    frame: false,
-  })
-
-  win.loadFile('index.html')
+function App() {
+  const window = require('./window');
+  const tray = require('./tray');
 }
 
-app.whenReady().then(() => createWindow());
+app.whenReady().then(App);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin')app.quit()
-})
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0)createWindow()
 })
